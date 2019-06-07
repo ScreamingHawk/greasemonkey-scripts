1d61b5a8f436df5691efb0b360396d0303837069
Michael Standen <screaminghawk@gmail.com>
2019-06-07 13:11:17 +1200

    Move video to top left. Fix metadata block

--- youtube-cinema.js
+++ youtube-cinema.js
@@ -1,31 +1,38 @@
 // ==UserScript==
-// @name     Alt+g Hides Youtube top bar
-// @version  1
-// @grant    none
+// @name Alt+g Youtube Cinema
+// @description Alt+g Hides search bar, moves player to top left
+// @version 1.1
+// @grant none
+// @match *youtube.com/*
+// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
 (function(){
 	var youtubeTopBarHidden = false;
 	document.addEventListener('keydown', function(e) {
-		// pressed alt+g
-		if (e.keyCode == 71 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
-			if (youtubeTopBarHidden){
-				// Clear
-				console.log('Unhiding');
-				document.getElementById('masthead-container').style.display = '';
-				document.getElementById('page-manager').style.marginTop = '';
-				var p = document.getElementById('primary');
-				p.style.padding = '';
-				p.style.margin = '';
-			} else {
-				console.log('Hiding');
-				document.getElementById('masthead-container').style.display = 'none';
-				document.getElementById('page-manager').style.margin = '0';
-				var p = document.getElementById('primary');
-				p.style.padding = '0';
-				p.style.margin = '0';
+		try {
+			// pressed alt+g
+			if (e.keyCode == 71 && !e.shiftKey && !e.ctrlKey && e.altKey && !e.metaKey) {
+				if (youtubeTopBarHidden){
+					// Clear
+					console.log('Unhiding');
+					$('div#masthead-container').show()
+					const p = $('div#player')
+					p.css('position', '')
+					p.css('top', '')
+					p.css('left', '')
+				} else {
+					console.log('Hiding');
+					$('div#masthead-container').hide()
+					const p = $('div#player')
+					p.css('position', 'absolute')
+					p.css('top', 0)
+					p.css('left', 0)
+				}
+				youtubeTopBarHidden = !youtubeTopBarHidden;
 			}
-			youtubeTopBarHidden = !youtubeTopBarHidden;
+		} catch (e){
+			console.error(e)
 		}
 	}, false);
 })();
